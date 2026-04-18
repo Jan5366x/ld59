dba58bba91dd6840c9c1e4a6b7572e6bed34806f
kormarun <kormarun@gmail.com>
2026-04-19 00:51:46 +0200

    simple radar

--- Assets/Scripts/Radar.cs
+++ Assets/Scripts/Radar.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Radar : MonoBehaviour
 {
+    public float speed = 10;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,20 @@ public class Radar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(0, 0, 1) * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var onScanPrefab = other.GetComponent<OnScan>();
+        if (onScanPrefab != null)
+        {
+            Instantiate(onScanPrefab.scanIndicatorPrefab, other.transform.position, Quaternion.identity);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log(other.name);
     }
 }
